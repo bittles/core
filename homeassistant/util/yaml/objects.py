a81e4b9ffb46effd2d79767abb5215bfe48e62c4
bittles <bauern.sc@gmail.com>
2026-07-01 17:54:18 -0500

    remove all folders but components

--- homeassistant/util/yaml/objects.py
+++
@@ -1,5 +0,0 @@
-"""Custom yaml object types."""
-
-from annotatedyaml import Input, NodeDictClass, NodeListClass, NodeStrClass
-
-__all__ = ["Input", "NodeDictClass", "NodeListClass", "NodeStrClass"]
